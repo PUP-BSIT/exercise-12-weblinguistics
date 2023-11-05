71e5526f58c6eb1108b815404c002c3c452ef459
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 17:33:06 +0800

    add Exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,20 +1,37 @@
-let commentForm = document.getElementById("comment_form");
 let nameInput = document.getElementById("name");
-let commentInput = document.getElementById("comment");
-let commentsList = document.getElementById("comments_list");
+let commentInput = document.getElementById("comment_text");
+let commentButton = document.getElementById("comment_button");
+let commentsSection = document.querySelector(".team-comment");
 
-commentForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-  
+nameInput.addEventListener("input", toggleCommentButton);
+commentInput.addEventListener("input", toggleCommentButton);
+
+commentButton.addEventListener("click", addComment);
+
+function toggleCommentButton() {
+    let nameValue = nameInput.value;
+    let commentValue = commentInput.value;
+
+    if (nameValue !== "" && commentValue !== "") {
+        commentButton.removeAttribute("disabled");
+    } else {
+        commentButton.setAttribute("disabled", "disabled");
+    }
+}
+
+function addComment() {
     let name = nameInput.value;
     let comment = commentInput.value;
-  
-    if (name && comment) {
-      let newComment = document.createElement("li");
-      newComment.textContent = `${name}: ${comment}`;
-      commentsList.appendChild(newComment);
-  
-      nameInput.value = "";
-      commentInput.value = "";
+
+    if (name.trim() === '' || comment.trim() === '') {
+        return;
     }
-});  
+
+let commentElement = document.createElement('div');
+commentElement.innerHTML = `<p><strong>Name:</strong> ${name}</p><p>${comment}</p>`;
+commentsSection.appendChild(commentElement);
+
+nameInput.value = '';
+commentInput.value = '';
+commentButton.setAttribute('disabled', 'true');
+}
