1c56f6741db786581953e69c0d1fdc299ee87187
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 15:58:56 +0800

    add Exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,22 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const commentButton = document.getElementById('comment_button');
-    const commentText = document.getElementById('comment_text');
-    const commentsContainer = document.getElementById('comments');
+const nameInput = document.getElementById('name');
+const commentInput = document.getElementById('comment');
+const commentButton = document.getElementById('comment_button');
 
-    commentButton.addEventListener('click', function () {
-        const name = document.getElementById('name').value;
-        const comment = commentText.value;
+function enableCommentButton() {
+    if (nameInput.value.trim() !== '' && commentInput.value.trim() !== '') {
+		commentButton.removeAttribute('disabled');
+    } else {
+		commentButton.setAttribute('disabled', 'disabled');
+    }
+}
 
-        if (name && comment) {
-            const newComment = document.createElement('div');
-            newComment.className = 'comment_container';
-            newComment.innerHTML = `<strong>Name: ${name}
-						</strong><br>${comment}<br><br>`;
-
-            commentsContainer.insertBefore(newComment, commentsContainer.firstChild);
-
-            document.getElementById('name').value = '';
-            commentText.value = '';
-        }
-    });
-});
+nameInput.addEventListener('input', enableCommentButton);
+commentInput.addEventListener('input', enableCommentButton);
