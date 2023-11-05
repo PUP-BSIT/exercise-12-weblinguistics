f22f833eae05cb94b56729d9f0f41820db33ae7e
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 17:37:57 +0800

    add Exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,37 +1,38 @@
-let nameInput = document.getElementById("name");
-let commentInput = document.getElementById("comment_text");
-let commentButton = document.getElementById("comment_button");
-let commentsSection = document.querySelector(".team-comment");
+<script>
+    let nameInput = document.getElementById("name");
+    let commentInput = document.getElementById("comment");
+    let commentButton = document.getElementById("comment-button");
+    let commentsSection = document.querySelector(".team-comment");
 
-nameInput.addEventListener("input", toggleCommentButton);
-commentInput.addEventListener("input", toggleCommentButton);
+    nameInput.addEventListener("input", toggleCommentButton);
+    commentInput.addEventListener("input", toggleCommentButton);
+    commentButton.addEventListener("click", addComment);
 
-commentButton.addEventListener("click", addComment);
+    function toggleCommentButton() {
+        let nameValue = nameInput.value;
+        let commentValue = commentInput.value;
 
-function toggleCommentButton() {
-    let nameValue = nameInput.value;
-    let commentValue = commentInput.value;
-
-    if (nameValue !== "" && commentValue !== "") {
-        commentButton.removeAttribute("disabled");
-    } else {
-        commentButton.setAttribute("disabled", "disabled");
-    }
-}
-
-function addComment() {
-    let name = nameInput.value;
-    let comment = commentInput.value;
-
-    if (name.trim() === '' || comment.trim() === '') {
-        return;
+        if (nameValue.trim() !== "" && commentValue.trim() !== "") {
+            commentButton.removeAttribute("disabled");
+        } else {
+            commentButton.setAttribute("disabled", "disabled");
+        }
     }
 
-let commentElement = document.createElement('div');
-commentElement.innerHTML = `<p><strong>Name:</strong> ${name}</p><p>${comment}</p>`;
-commentsSection.appendChild(commentElement);
+    function addComment() {
+        let name = nameInput.value;
+        let comment = commentInput.value;
 
-nameInput.value = '';
-commentInput.value = '';
-commentButton.setAttribute('disabled', 'true');
-}
+        if (name.trim() === '' || comment.trim() === '') {
+            return;
+        }
+
+        let commentElement = document.createElement('div');
+        commentElement.innerHTML = `<h4>${name}</h4><p>${comment}</p>`;
+        commentsSection.appendChild(commentElement);
+
+        nameInput.value = '';
+        commentInput.value = '';
+        commentButton.setAttribute('disabled', 'true');
+    }
+</script>
