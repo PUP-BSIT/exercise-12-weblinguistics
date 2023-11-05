3a91e6deadf2154426620a6cb2842aa63f67dc11
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 17:27:20 +0800

    add exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,40 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const nameInput = document.getElementById("name");
-    const commentInput = document.getElementById("comment");
-    const commentButton = document.getElementById("comment_button");
-    const commentsList = document.getElementById("comments_list");
+let commentForm = document.getElementById("comment_form");
+let nameInput = document.getElementById("name");
+let commentInput = document.getElementById("comment");
+let commentsList = document.getElementById("comments_list");
 
-    nameInput.addEventListener("input", updateCommentButtonState);
-    commentInput.addEventListener("input", updateCommentButtonState);
-    commentButton.addEventListener("click", addComment);
-
-    function updateCommentButtonState() {
-        if (nameInput.value.trim() !== "" && commentInput.value.trim() !== "") {
-            commentButton.disabled = false;
-        } else {
-            commentButton.disabled = true;
-        }
+commentForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    let name = nameInput.value;
+    let comment = commentInput.value;
+  
+    if (name && comment) {
+      let newComment = document.createElement("li");
+      newComment.textContent = `${name}: ${comment}`;
+      commentsList.appendChild(newComment);
+  
+      nameInput.value = "";
+      commentInput.value = "";
     }
-
-    function addComment(event) {
-        event.preventDefault();
-
-        const name = nameInput.value.trim();
-        const comment = commentInput.value.trim();
-
-        if (name === "" || comment === "") {
-            return;
-        }
-
-        const newComment = document.createElement("li");
-        const commentText = document.createElement("p");
-        commentText.textContent = `${name}: ${comment}`;
-        newComment.appendChild(commentText);
-        commentsList.appendChild(newComment);
-
-        // Clear input fields after adding the comment
-        nameInput.value = "";
-        commentInput.value = "";
-        commentButton.disabled = true;
-    }
-});
+});  
