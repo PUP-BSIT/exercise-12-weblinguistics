9e9dcbffc7bedd6e33a0df007ab9444e4efd0db3
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 17:20:09 +0800

    add Exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,22 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const commentButton = document.getElementById('comment_button');
-    const commentText = document.getElementById('comment_text');
-    const commentsContainer = document.getElementById('comments');
+document.addEventListener("DOMContentLoaded", function () {
+    const nameInput = document.getElementById("name");
+    const commentInput = document.getElementById("comment");
+    const commentButton = document.getElementById("comment_button");
+    const commentsList = document.getElementById("comments_list");
 
-    commentButton.addEventListener('click', function () {
-        const name = document.getElementById('name').value;
-        const comment = commentText.value;
+    nameInput.addEventListener("input", updateCommentButtonState);
+    commentInput.addEventListener("input", updateCommentButtonState);
+    commentButton.addEventListener("click", addComment);
 
-        if (name && comment) {
-            const newComment = document.createElement('div');
-            newComment.className = 'comment_container';
-            newComment.innerHTML = `<strong>Name: ${name}
-						</strong><br>${comment}<br><br>`;
-
-            commentsContainer.insertBefore(newComment, commentsContainer.firstChild);
-
-            document.getElementById('name').value = '';
-            commentText.value = '';
+    function updateCommentButtonState() {
+        if (nameInput.value.trim() !== "" && commentInput.value.trim() !== "") {
+            commentButton.disabled = false;
+        } else {
+            commentButton.disabled = true;
         }
-    });
+    }
+
+    function addComment(event) {
+        event.preventDefault();
+
+        const name = nameInput.value.trim();
+        const comment = commentInput.value.trim();
+
+        if (name === "" || comment === "") {
+            return;
+        }
+
+        const newComment = document.createElement("li");
+        const commentText = document.createElement("p");
+        commentText.textContent = `${name}: ${comment}`;
+        newComment.appendChild(commentText);
+        commentsList.appendChild(newComment);
+
+        // Clear input fields after adding the comment
+        nameInput.value = "";
+        commentInput.value = "";
+        commentButton.disabled = true;
+    }
 });
