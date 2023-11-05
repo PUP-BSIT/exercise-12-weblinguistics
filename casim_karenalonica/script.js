14211625df3d9048d91689e2926ad8f5dfde520a
Karen Alonica Casim <karennicacasim.1124@gmail.com>
2023-11-05 16:42:08 +0800

    add Exercise 10 files

--- casim_karenalonica/script.js
+++ casim_karenalonica/script.js
@@ -1,23 +1,21 @@
-const nameInput = document.getElementById('name');
-        const commentInput = document.getElementById('comment');
-        const commentButton = document.getElementById('comment_button');
-        const commentsContainer = document.getElementById('comments');
+document.addEventListener('DOMContentLoaded', function () {
+    const commentButton = document.getElementById('comment_button');
+    const commentText = document.getElementById('comment'); 
+    const commentsContainer = document.getElementById('comments');
 
-        commentButton.addEventListener('click', function () {
-            const name = nameInput.value;
-            const comment = commentInput.value;
+    commentButton.addEventListener('click', function () {
+        const name = document.getElementById('name').value;
+        const comment = commentText.value;
 
-            if (name && comment) {
-                // Create a new comment element
-                const commentElement = document.createElement('div');
-                commentElement.className = 'comment';
-                commentElement.innerHTML = `<strong>${name}:</strong> ${comment}`;
+        if (name && comment) {
+            const newComment = document.createElement('div');
+            newComment.className = 'comment_container';
+            newComment.innerHTML = `<strong>Name: ${name}</strong><br>${comment}<br><br>`;
 
-                // Append the new comment to the comments container
-                commentsContainer.appendChild(commentElement);
+            commentsContainer.insertBefore(newComment, commentsContainer.firstChild);
 
-                // Clear the input fields
-                nameInput.value = '';
-                commentInput.value = '';
-            }
-        });
+            document.getElementById('name').value = '';
+            commentText.value = '';
+        }
+    });
+});
